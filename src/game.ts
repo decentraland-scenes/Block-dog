0903a7fae918ccfb7fc81c8c9c855b060c0882ad
nearnshaw <nearnshaw@decentraland.org>
2020-01-14 14:12:43 -0300

    hover feedback

--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,5 @@
-import { LerpData, LerpMove } from "./modules/walk";
-import { SwitchGoals, Behavior, Goal, setDogGoal } from "./modules/switchGoals";
-
+import { LerpData, LerpMove } from './modules/walk'
+import { SwitchGoals, Behavior, Goal, setDogGoal } from './modules/switchGoals'
 
 // camera object to get user position
 export const camera = Camera.instance
@@ -12,9 +11,6 @@ engine.addSystem(new LerpMove())
 
 ////////////////////////
 //OTHER FUNCTIONS
-
-
-
 
 export function setAnimations(dog: IEntity) {
   let sit = dog.getComponent(Animator).getClip('Sitting_Armature_0')
@@ -31,8 +27,8 @@ export function setAnimations(dog: IEntity) {
 
   switch (dog.getComponent(Behavior).goal) {
     case Goal.Sit:
-	  sit.playing = true
-	  sit.looping = false
+      sit.playing = true
+      sit.looping = false
       break
     case Goal.Follow:
       walk.playing = true
@@ -47,8 +43,8 @@ export function setAnimations(dog: IEntity) {
       break
   }
   if (dog.getComponent(Behavior).previousGoal == Goal.Sit) {
-	stand.playing = true
-	sit.looping = false
+    stand.playing = true
+    sit.looping = false
   }
 }
 
@@ -58,26 +54,33 @@ export function setAnimations(dog: IEntity) {
 // Bowl
 const bowl = new Entity()
 bowl.addComponent(new GLTFShape('models/BlockDogBowl.gltf'))
-bowl.addComponent(new Transform({
-  position: new Vector3(9, 0, 1)
-}))
 bowl.addComponent(
-  new OnClick(e => {
-    setDogGoal(dog, Goal.GoDrink)
-    dog.getComponent(LerpData).target = bowl.getComponent(Transform).position
-    dog.getComponent(LerpData).origin = dog.getComponent(Transform).position
-    dog.getComponent(LerpData).fraction = 0
+  new Transform({
+    position: new Vector3(9, 0, 1)
   })
+)
+bowl.addComponent(
+  new OnPointerDown(
+    e => {
+      setDogGoal(dog, Goal.GoDrink)
+      dog.getComponent(LerpData).target = bowl.getComponent(Transform).position
+      dog.getComponent(LerpData).origin = dog.getComponent(Transform).position
+      dog.getComponent(LerpData).fraction = 0
+    },
+    { button: ActionButton.POINTER, hoverText: 'Go Drink' }
+  )
 )
 engine.addEntity(bowl)
 
 // Garden
 const garden = new Entity()
 garden.addComponent(new GLTFShape('models/garden.glb'))
-garden.addComponent(new Transform({
-  position: new Vector3(8, 0, 8),
-  scale: new Vector3(1.6, 1.6, 1.6)
-}))
+garden.addComponent(
+  new Transform({
+    position: new Vector3(8, 0, 8),
+    scale: new Vector3(1.6, 1.6, 1.6)
+  })
+)
 engine.addEntity(garden)
 
 // Dog
@@ -88,23 +91,29 @@ let idleAnimation = new AnimationState('Idle_Armature_0')
 
 dog.getComponent(Animator).addClip(idleAnimation)
 
-dog.getComponent(Animator)
+dog
+  .getComponent(Animator)
   .getClip('Idle')
   .play()
 
-dog.addComponent(new Transform({
-  position: new Vector3(5, 0, 5)
-}))
+dog.addComponent(
+  new Transform({
+    position: new Vector3(5, 0, 5)
+  })
+)
 dog.addComponent(new Behavior())
 dog.addComponent(new LerpData())
 dog.addComponent(
-  new OnClick(e => {
-    if (dog.getComponent(Behavior).goal == Goal.Sit) {
-	  setDogGoal(dog, Goal.Idle)
-    } else {
-      setDogGoal(dog, Goal.Sit)
-      dog.getComponent(LerpData).fraction = 1
-    }
-  })
+  new OnPointerDown(
+    e => {
+      if (dog.getComponent(Behavior).goal == Goal.Sit) {
+        setDogGoal(dog, Goal.Idle)
+      } else {
+        setDogGoal(dog, Goal.Sit)
+        dog.getComponent(LerpData).fraction = 1
+      }
+    },
+    { button: ActionButton.POINTER, hoverText: 'Sit' }
+  )
 )
 engine.addEntity(dog)
